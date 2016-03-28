9a371ca4829f964a0d9f2faa32d1a9050767fed6
Saranga Abeykoon <amisaranga@gmail.com>
2016-03-28 08:50:36 +0530

    Service provider class name issue fixed.

--- src/ListViewServiceProvider.php
+++ src/ListViewServiceProvider.php
@@ -5,14 +5,14 @@ namespace Nterms\ListView;
 use Blade;
 use Illuminate\Support\ServiceProvider;
 
-class ListViewServicePorvider extends ServiceProvider
+class ListViewServiceProvider extends ServiceProvider
 {
     /**
      * Indicates of the loding of the provider is deffered.
      * 
      * @var bool
      */
-    protected $defer = true;
+    //protected $defer = true;
     
     /**
      * Perform post-registration booting of services
@@ -21,18 +21,52 @@ class ListViewServicePorvider extends ServiceProvider
      */
     public function boot()
     {
-        Blade::directive('listview', function($view, $items, $itemName, $viewEmpty = '', $options = []) {
-            $html = '';
+        Blade::directive('listview', function($expression) {
             
-            if(count($items)) {
-                foreach($items as $item) {
-                    $html .= view($view, [$itemName => $item]);
-                }
+            $args = explode(',', str_replace(['(', ')', ' '], '', $expression));
+            $view = ''; $items = []; $itemName = ''; $viewEmpty = ''; $options = [];
+            
+            if(isset($args[0])) {
+                $view = $args[0];
             } else {
-                $html = view($viewEmpty);
+                abort(422, "Missing 1st argument 'view' for @listview");
             }
             
-            return $html; 
+            if(isset($args[1])) {
+                $items = $args[1];
+            } else {
+                abort(422, "Missing 2nd argument 'items' for @listview");
+            }
+            
+            if(isset($args[2])) {
+                $itemName = $args[2];
+            } else {
+                abort(422, "Missing 3rd argument 'itemName' for @listview");
+            }
+            
+            if(isset($args[3])) {
+                $viewEmpty = $args[3];
+            }
+            
+            if(isset($args[4])) {
+                $options = $args[4];
+            }
+            
+            $php = '<?php
+                if(!empty(' . $items . ')) {
+                    foreach('. $items . ' as $item) {
+                        echo view(' . $view . ', [' . $itemName . ' => $item]);
+                    }
+                }
+            ';
+            
+            if(!empty($viewEmpty)) {
+                $php .= ' else { echo view(' . $viewEmpty . '); }';
+            }
+            
+            $php .= ' ?>';
+            
+            return $php; 
         });
     }
     
